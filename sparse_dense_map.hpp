2a1ac4baef5bcf595b1cccf3f60410f981b8182b
Mateo Membreno <membreno@umich.edu>
2026-06-09 11:08:55 -0400

    resize, insert, fix bugs contains get

--- sparse_dense_map.hpp
+++ sparse_dense_map.hpp
@@ -25,32 +25,51 @@ public:
 
     ~SparseDenseMap() = default;
 
-    insert(Key key, Value value){
-
-    }
-
-    delete(Key key){
-
-    }
-
-    bool contains(Key key){
-        if(Hash{}(key) == EMPTY){
-            return false;
+    void insert(const Key& key, const Value& value){
+        size_t index = Hash{}(key) % sparse_.size();
+        while(sparse_[index] != EMPTY && sparse_[index] != TOMB){
+            if(KeyEqual{}(key, dense_[sparse_[index]].first)){
+                dense_[sparse_[index]].second = value;
+                return;
+            }
+             index = (index + 1) % sparse_.size();
         }
-        return true;
+        sparse_[index] = size_;
+        dense_.emplace_back(key, value);
+        size_++;
+        if(size_ * 2 > sparse_.size()){
+            resize();
+        }
+    }
+
+    // think of append method if workds for generik value type
+
+    void remove(Key key){
+        return;
+    }
+
+    bool contains(const Key& key){
+        size_t index = Hash{}(key) % sparse_.size();
+        while(sparse_[index] != EMPTY){
+            if(sparse_[index] != TOMB && KeyEqual{}(key, dense_[sparse_[index]].first)){
+                return true;
+            }
+            index = (index + 1) % sparse_.size();
+        }
+        return false;
     }
 
     Value get(const Key& key){
-        Hash hs();
-        size_t index = hs(key) % sparse_.size();
+        size_t index = Hash{}(key) % sparse_.size();
         while(sparse_[index] != EMPTY){
-             if (KeyEqual(key, dense[sparse_[index]].first)){
-                return dense[sparse_[index].second];
+             if (sparse_[index] != TOMB && KeyEqual{}(key, dense_[sparse_[index]].first)){
+                return dense_[sparse_[index]].second;
              }
-             index++;
+             index = (index + 1) % sparse_.size();
         }
         throw std::out_of_range("key not found");
     }
+
 private:
     static constexpr size_t EMPTY = SIZE_MAX;
     static constexpr size_t TOMB = SIZE_MAX - 1;
@@ -59,4 +78,16 @@ private:
     std::vector<std::pair<const Key, Value>> dense_;
     size_t size_;
 
+    void resize(){
+        sparse_.assign(sparse_.size() * 2, EMPTY);
+        size_t index;
+        for (size_t i = 0; i < size_; i++){
+            index = Hash{}(dense_[i].first) % sparse_.size();
+            while(sparse_[index] != EMPTY){
+                index = (index + 1) % sparse_.size();
+            }
+            sparse_[index] = i;
+
+        }
+    }
 };
